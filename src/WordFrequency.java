7f4ac5fc50980721b4d148f6e0c28b8244410b98
DavidonMac <rachealsmac@192.168.1.15>
2019-10-30 21:24:23 -0700

    Fixed the file reading so it doesn't throw an error for filenotfound

--- src/WordFrequency.java
+++ src/WordFrequency.java
@@ -1,8 +1,35 @@
+import java.util.*;
+import java.io.*;
+
 
 public class WordFrequency {
-
-	public static void main(String[] args) {
-
+	private static int lineCount, wordCount;
+	public static void main(String[] args) throws FileNotFoundException{
+		WordFrequency wf = new WordFrequency();
+		lineCount = 0;
+		wordCount = 0;
+		wf.readFile("Pride and Prejudice.txt");
 	}
 
+	private void readFile(String filename) throws FileNotFoundException {
+		Scanner reader = new Scanner(new File(filename));
+		
+		while (reader.hasNextLine()) {
+			String line = reader.nextLine();
+			lineCount++;
+			System.out.println("Num of lines are " + lineCount);
+
+			Scanner lineReader = new Scanner(line);
+			while(lineReader.hasNext()) {
+				
+				String word = lineReader.next();
+				wordCount++;
+
+			}
+			
+		}
+		System.out.println("Num of lines are " + lineCount);
+		System.out.println("Num of words are " + wordCount);
+		
+	}
 }
